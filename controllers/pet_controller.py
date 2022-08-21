28a1ee209f32ad530a4c0e00759625744837e9dd
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-21 19:34:13 +0100

    integrated the owner class

--- controllers/pet_controller.py
+++ controllers/pet_controller.py
@@ -5,6 +5,7 @@ from models.pet import Pet
 
 import repositories.pet_repository as pet_repository
 import repositories.vet_repository as vet_repository
+import repositories.owner_repository as owner_repository
 
 pets_blueprint = Blueprint("pets", __name__)
 
@@ -22,7 +23,8 @@ def get_pet(id):
 def edit_pet(id):
     pet = pet_repository.select(id)
     vets = vet_repository.select_all()
-    return render_template('pets/edit.html', pet = pet, vets = vets)
+    owners = owner_repository.select_all()
+    return render_template('pets/edit.html', pet = pet, vets = vets, owners = owners)
 
 @pets_blueprint.route("/pets/<id>", methods = ['POST'])
 def update_pet(id):
@@ -30,19 +32,21 @@ def update_pet(id):
     date_of_birth = request.form['date_of_birth']
     pet_type = request.form['pet_type']
     breed = request.form['breed']
-    pet_owner = request.form['pet_owner']
     treatment_notes = request.form['treatment_notes']
     vet_id = request.form['vet_id']
+    owner_id = request.form['owner_id']
+    owner = owner_repository.select(owner_id)
     vet = vet_repository.select(vet_id)
-    pet = Pet(pet_name, date_of_birth, pet_type, breed, pet_owner, treatment_notes, vet, id)
+    pet = Pet(pet_name, date_of_birth, pet_type, breed, owner, treatment_notes, vet, id)
     pet_repository.update(pet)
     print (pet)
     return redirect('/pets')
 
 @pets_blueprint.route('/pets/new', methods = ['GET'])
 def new_pet():
+    owners = owner_repository.select_all()
     vets = vet_repository.select_all()
-    return render_template ("/pets/new.html", vets = vets)
+    return render_template ("/pets/new.html", vets = vets, owners = owners)
 
 @pets_blueprint.route('/pets/new', methods = ['POST'])
 def create_new_pet():
@@ -50,11 +54,12 @@ def create_new_pet():
     date_of_birth = request.form ['date_of_birth']
     pet_type = request.form['pet_type']
     breed = request.form['breed']
-    pet_owner = request.form['pet_owner']
+    owner_id = request.form['owner_id']
     vet_id = request.form['vet_id']
     treatment_notes = request.form['treatment_notes']
+    owner = owner_repository.select(owner_id)
     vet = vet_repository.select(vet_id)
-    pet = Pet(pet_name, date_of_birth, pet_type, breed, pet_owner, treatment_notes, vet)
+    pet = Pet(pet_name, date_of_birth, pet_type, breed, owner, treatment_notes, vet)
     pet_repository.save(pet)
     return redirect('/pets')
 
